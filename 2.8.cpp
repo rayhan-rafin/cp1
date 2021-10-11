e645fe4b7cd9091a6a5c6d875653c302f78ac4a7
rayhan-rafin <92117578+rayhan-rafin@users.noreply.github.com>
2021-10-12 00:16:15 +0600

    Add files via upload

---
+++ 2.8.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main ()
+{
+    int a;
+    a = 1000;
+    printf ("value of a is %d\n",a);
+
+    a = -21000;
+    printf ("value of a is %d\n",a);
+
+    a = 10000000;
+    printf ("value of a is %d\n",a);
+
+    a = -10000000;
+    printf ("value of a is %d\n",a);
+
+    a = 1000200030004000500;
+    printf ("value of a is %d\n",a);
+
+    a = - 12345678910;
+    printf ("value of a is %d\n",a);
+
+     return 0;
+}
