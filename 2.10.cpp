feaebbd3c7d4a480b1e25e5f6445dfb635016f48
rayhan-rafin <92117578+rayhan-rafin@users.noreply.github.com>
2021-10-15 01:30:49 +0600

    Add files via upload

---
+++ 2.10.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main ()
+{
+    int a,b,sum;
+
+    printf ("enter value of a \na = ");
+    scanf("%d", &a);
+
+    printf ("enter value of b \nb = ");
+    scanf ("%d", &b);
+
+    sum = a+b;
+
+    printf ("sum is %d",sum);
+
+    return 0;
+
+}
