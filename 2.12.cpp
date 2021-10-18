51fac4ad346a643fcfebffd3607c2eb6fb39a5fc
rayhan-rafin <92117578+rayhan-rafin@users.noreply.github.com>
2021-10-18 21:17:25 +0600

    Add files via upload

--- 2.12.cpp
+++ 2.12.cpp
@@ -5,16 +5,20 @@ int main()
 {
     char a,b,c;
 
-    printf (" shorten your name to 3 letter \nEnter the first letter :");
-    scanf(" %c",&a);
+    printf ("shorten your name to 3 letter \nEnter the first letter :");
+    scanf("%c",&a);
+    getchar();
 
     printf ("Enter the second letter :");
-    scanf(" %c",&b);
+    scanf("%c",&b);
+    getchar();
+
 
     printf ("Enter the third letter :");
-    scanf (" %c",&c);
+    scanf ("%c",&c);
+    getchar();
 
-    printf (" Your shortened name is %c%c%c", a, b, c);
+    printf ("Your shortened name is %c%c%c", a, b, c);
 
     return 0;
 }
