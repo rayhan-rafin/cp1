0065b83e2ab02555ef9f91dcda6c1b9946449bfd
rayhan-rafin <92117578+rayhan-rafin@users.noreply.github.com>
2021-10-21 01:28:06 +0600

    Add files via upload

---
+++ 3.1.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+
+{
+    int a;
+    printf("Enter a number(Either positive and negative)\nThe number :");
+    scanf ("%d",&a);
+
+    if (a>=0)
+    {
+        printf("The number is positive\n");
+    }
+
+    else
+    {
+        printf("The number is neagtive\n");
+    }
+
+    return 0;
+}
---
+++ 3.3.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+
+{
+    int a;
+    printf("Enter a number(Either positive and negative)\nThe number :");
+    scanf ("%d",&a);
+
+    if (a >= 0)
+    {
+        printf("The number is positive\n");
+    }
+
+    else if (a == 0)
+    {
+        printf ("The number is zero\n");
+    }
+
+    else if (a <= 0)
+    {
+        printf("The number is neagtive\n");
+    }
+
+    return 0;
+}
+
